67b8d4897bc45860e8934f3f6e38ef936b4d23db
20gtrue <79179208+20gtrue@users.noreply.github.com>
2023-04-20 16:33:19 -0700

    Update and rename pub.py to tcp_server.c

---
+++ tcp_server.c
@@ -0,0 +1,20 @@
+import socket
+
+HOST = '127.0.0.1' # The server's hostname or IP address
+PORT = 1234 # The port used by the server
+
+# Create a socket object
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    # Bind the socket to a specific address and port
+    s.bind((HOST, PORT))
+    # Listen for incoming connections
+    s.listen()
+    print('Server listening on', HOST, PORT)
+    # Accept a connection
+    conn, addr = s.accept()
+    print('Connected by', addr)
+    # Receive data from the client
+    data = conn.recv(1024)
+    print('Received', repr(data))
+    # Send a response back to the client
+    conn.sendall(b'Hello, client!')
